10e8f3a17261c41e5b2e51c84e4c7cdc4cbb375c
Tsuki <695881164@qq.com>
2021-03-20 14:03:39 +0800

    INIT

---
+++ src/main/java/com/just/ekao/EkaoApplication.java
@@ -0,0 +1,13 @@
+package com.just.ekao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EkaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/just/ekao/EkaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.just.ekao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
