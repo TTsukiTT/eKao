b85a1b7cb5ca47ecc3b521abfad783f079308a3a
Tsuki <695881164@qq.com>
2021-03-21 22:28:06 +0800

    INIT

---
+++ src/main/java/com/just/ekao/sys/pub/controller/PageController.java
@@ -0,0 +1,12 @@
+package com.just.ekao.sys.pub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("/login")
+    public String toLogin(){
+        return "login";
+    }
+}
